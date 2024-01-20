bedce78a70b7829b76aca92d1002ea7883507726
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-01-20 23:29:36 +0100

    DatePicker implemented

--- app/src/main/java/com/example/formstudent/MainActivity.kt
+++ app/src/main/java/com/example/formstudent/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.formstudent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.inputmethod.InputMethodManager
+import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.example.formstudent.databinding.ActivityMainBinding
 import com.example.formstudent.viewModel.MainViewModel
 import com.google.android.material.snackbar.Snackbar
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,6 +27,23 @@ class MainActivity : AppCompatActivity() {
             clearFocus()
         }
 
+        binding.etBornDate.setOnFocusChangeListener { _, hasFocus -> if(hasFocus){showDatePickerDialog()} }
+
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+
+    fun onDateSelected(day: Int, month: Int, year: Int){
+        binding.etBornDate.setText("$day/$month/$year")
+
+
+        if(Calendar.getInstance().get(Calendar.YEAR) - year < 18){
+            binding.tilSchool.isVisible = true
+
+        }else {binding.tilSchool.visibility = android.view.View.INVISIBLE}
     }
 
     private fun clearFocus() {
